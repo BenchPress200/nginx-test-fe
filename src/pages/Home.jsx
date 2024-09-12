a1f7b67278937422e4892102a42081eb986edf6d
BenchPress200 <jms0538@naver.com>
2024-09-12 19:59:12 +0900

    chore

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import { useNavigate } from 'react-router-dom';
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState } from "react";
 import styles from '../styles/Home.module.css'
 
 const Home = () => {
